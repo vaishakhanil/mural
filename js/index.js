06b13aa648d3172567554da09f43ada43bc34c06
TheTrident <vaishakh.anil.360@gmail.com>
2018-02-27 00:21:51 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+function ide() {
+		// body...
+	document.getElementById('logo').style.visibility="visible";
+	document.getElementById('about').style.visibility="hidden";
+	document.getElementById('work').style.visibility="hidden";
+	document.getElementById('port').style.visibility="hidden";
+	document.getElementById('contact').style.visibility="hidden";
+}
+
+
+
+
+
+function about() {
+		// body...
+	document.getElementById('logo').style.visibility="hidden";
+	document.getElementById('about').style.visibility="visible";
+	document.getElementById('work').style.visibility="hidden";
+	document.getElementById('port').style.visibility="hidden";
+	document.getElementById('contact').style.visibility="hidden";
+	
+}
+
+function work() {
+		// body...
+	document.getElementById('logo').style.visibility="hidden";
+	document.getElementById('about').style.visibility="hidden";
+	document.getElementById('work').style.visibility="visible";
+	document.getElementById('port').style.visibility="hidden";
+	document.getElementById('contact').style.visibility="hidden";
+	
+}
+
+function port() {
+		// body...
+	document.getElementById('logo').style.visibility="hidden";
+	document.getElementById('about').style.visibility="hidden";
+	document.getElementById('work').style.visibility="hidden";
+	document.getElementById('port').style.visibility="visible";
+	document.getElementById('contact').style.visibility="hidden";
+	
+}
+
+function contact() {
+		// body...
+	document.getElementById('logo').style.visibility="hidden";
+	document.getElementById('about').style.visibility="hidden";
+	document.getElementById('work').style.visibility="hidden";
+	document.getElementById('port').style.visibility="hidden";
+	document.getElementById('contact').style.visibility="visible";
+	
+}
+
